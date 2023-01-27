b828c0d5ed200f5f3c3c2786b85d61bb13efeab3
YaswanthVarma754 <konduru.varma@tringapps.com>
2023-01-27 23:33:24 +0530

    changed to logger

--- src/main/java/org/example/MyBankAccount.java
+++ src/main/java/org/example/MyBankAccount.java
@@ -8,6 +8,7 @@ public class MyBankAccount {
     private String name;
     private double accNum;
     private double balance;
+    private String s;
 
     MyBankAccount(String name, double accNum, double balance) {
         this.name = name;
@@ -17,18 +18,24 @@ public class MyBankAccount {
 
     void deposit(int money) {
         balance += money;
-        logger.info("Money credited to your account : " + money + ".");
-        logger.info("Current Balance : " + balance + ".");
+        s = "Money credited to your account : " + money + ".";
+        logger.info(s);
+        s = "Current Balance : " + balance + ".";
+        logger.info(s);
     }
 
     void withdraw(int money) {
         if (balance < money) {
-            logger.info("Your balance is too low to withdraw the amount you requested.");
-            logger.info("Your current balance : " + balance + ". But you requested " + money + ".");
+            s = "Your balance is too low to withdraw the amount you requested.";
+            logger.info(s);
+            s = "Your current balance : " + balance + ". But you requested " + money + ".";
+            logger.info(s);
         } else {
             balance -= money;
-            logger.info("Money debited from your account : " + money + ".");
-            logger.info("Current Balance : " + balance + ".");
+            s = "Money debited from your account : " + money + ".";
+            logger.info(s);
+            s = "Current Balance : " + balance + ".";
+            logger.info(s);
         }
     }
 
@@ -49,6 +56,7 @@ class Main {
         boolean flag = true;
         double accNum;
         double balance;
+        String s;
         int choice;
         Scanner sc = new Scanner(System.in);
         logger.info("Enter Customer name : ");
@@ -58,12 +66,14 @@ class Main {
         logger.info("Enter initial Balance : ");
         balance = sc.nextDouble();
         MyBankAccount bb = new MyBankAccount(name, accNum, balance);
-        logger.info("Welcome " + bb.getName() + ".");
-        logger.info("Your Account balance when your account is created : " + bb.balance() + ".");
+        s = "Welcome " + bb.getName() + ".";
+        logger.info(s);
+        s = "Your Account balance when your account is created : " + bb.balance() + ".";
+        logger.info(s);
         while (flag) {
             int money;
-            logger.info("");
-            logger.info("1. Deposit" + "\n2. Withdraw" + "\n3. Balance" + "\n4. Exit" + "\nEnter your choice : ");
+            s = "1. Deposit" + "\n2. Withdraw" + "\n3. Balance" + "\n4. Exit" + "\nEnter your choice : ";
+            logger.info(s);
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
@@ -77,7 +87,8 @@ class Main {
                     bb.withdraw(money);
                     break;
                 case 3:
-                    logger.info("Your Balance is : " + bb.balance() + ".");
+                    s = "Your Balance is : " + bb.balance() + ".";
+                    logger.info(s);
                     break;
                 default:
                     flag = false;
