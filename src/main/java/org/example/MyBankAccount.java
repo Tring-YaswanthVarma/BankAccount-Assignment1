0938282c7fbd74946958573692a42329cb94757a
YaswanthVarma754 <konduru.varma@tringapps.com>
2023-01-27 23:25:39 +0530

    changed variables names

--- src/main/java/org/example/MyBankAccount.java
+++ src/main/java/org/example/MyBankAccount.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 public class MyBankAccount {
+    Logger logger = Logger.getLogger("com.api.jar");
     private String name;
     private double accNum;
     private double balance;
@@ -15,18 +17,18 @@ public class MyBankAccount {
 
     void deposit(int money) {
         balance += money;
-        System.out.println("Money credited to your account : " + money + ".");
-        System.out.println("Current Balance : " + balance + ".");
+        logger.info("Money credited to your account : " + money + ".");
+        logger.info("Current Balance : " + balance + ".");
     }
 
     void withdraw(int money) {
         if (balance < money) {
-            System.out.println("Your balance is too low to withdraw the amount you requested.");
-            System.out.println("Your current balance : " + balance + ". But you requested " + money + ".");
+            logger.info("Your balance is too low to withdraw the amount you requested.");
+            logger.info("Your current balance : " + balance + ". But you requested " + money + ".");
         } else {
             balance -= money;
-            System.out.println("Money debited from your account : " + money + ".");
-            System.out.println("Current Balance : " + balance + ".");
+            logger.info("Money debited from your account : " + money + ".");
+            logger.info("Current Balance : " + balance + ".");
         }
     }
 
@@ -42,40 +44,40 @@ public class MyBankAccount {
 
 class Main {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger("com.api.jar");
         String name;
         boolean flag = true;
         double accNum;
         double balance;
         int choice;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Customer name : ");
+        logger.info("Enter Customer name : ");
         name = sc.nextLine();
-        System.out.println("Enter Account number : ");
+        logger.info("Enter Account number : ");
         accNum = sc.nextDouble();
-        System.out.println("Enter initial Balance : ");
+        logger.info("Enter initial Balance : ");
         balance = sc.nextDouble();
         MyBankAccount bb = new MyBankAccount(name, accNum, balance);
-        System.out.println("Welcome " + bb.getName() + ".");
-        System.out.println("Your Account balance when your account is created : " + bb.balance() + ".");
+        logger.info("Welcome " + bb.getName() + ".");
+        logger.info("Your Account balance when your account is created : " + bb.balance() + ".");
         while (flag) {
             int money;
-            System.out.println();
-            System.out
-                    .println("1. Deposit" + "\n2. Withdraw" + "\n3. Balance" + "\n4. Exit" + "\nEnter your choice : ");
+            logger.info("");
+            logger.info("1. Deposit" + "\n2. Withdraw" + "\n3. Balance" + "\n4. Exit" + "\nEnter your choice : ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter money to deposit in your account : ");
+                    logger.info("Enter money to deposit in your account : ");
                     money = sc.nextInt();
                     bb.deposit(money);
                     break;
                 case 2:
-                    System.out.println("Enter money to withdraw from your account : ");
+                    logger.info("Enter money to withdraw from your account : ");
                     money = sc.nextInt();
                     bb.withdraw(money);
                     break;
                 case 3:
-                    System.out.println("Your Balance is : " + bb.balance() + ".");
+                    logger.info("Your Balance is : " + bb.balance() + ".");
                     break;
                 default:
                     flag = false;
